93a36f110cfe97d1d6fcf47e9d582740add048ee
Andy Ye <ye.andyy@gmail.com>
2021-06-08 19:36:10 -0400

    Added calc button functionality, style to calc

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log('ssdsf');
 
+let displayVal = 0;
+const displayMaxLen = 8;
+
 
 function add(a,b){
     return a+b;
@@ -32,4 +35,20 @@ function operate(num1, num2, operator){
     }
 }
 
-console.log(operate(3,4,'+'));
+const numberBtns = document.querySelectorAll('.number');
+const numDisplay = document.querySelector('#numdisplay');
+numberBtns.forEach((button) => { button.addEventListener('click', setDisplayNumber) });
+
+function setDisplayNumber(){ //concate strings to display >1 button presses
+    if (displayVal==0){ //show 0 at start, replace with firstNumButton clicked.
+        numDisplay.textContent="";
+        displayVal="";
+    }
+    if (displayVal.length>displayMaxLen){ //0 index strings;
+        return;
+    }
+    displayVal+= this.textContent;
+    numDisplay.textContent+= this.textContent;
+    console.log(displayVal);
+}
+
